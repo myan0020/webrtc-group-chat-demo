83699ec44316108d68d5613e4c866ee53c0b5b6b
yangming <843670891@qq.com>
2023-01-09 23:15:54 +0800

    webpack resolve issue

--- react_client/hook/use-beforeunload.js
+++ react_client/hook/use-beforeunload.js
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
-const useBeforeunload = (handler) => {
+export default function useBeforeunload(handler) {
   const eventListenerRef = useRef();
 
   useEffect(() => {
@@ -8,25 +8,23 @@ const useBeforeunload = (handler) => {
       const returnValue = handler?.(event);
       // Handle legacy `event.returnValue` property
       // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
-      if (typeof returnValue === 'string') {
+      if (typeof returnValue === "string") {
         return (event.returnValue = returnValue);
       }
       // Chrome doesn't support `event.preventDefault()` on `BeforeUnloadEvent`,
       // instead it requires `event.returnValue` to be set
       // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload#browser_compatibility
       if (event.defaultPrevented) {
-        return (event.returnValue = '');
+        return (event.returnValue = "");
       }
     };
   }, [handler]);
 
   useEffect(() => {
     const eventListener = (event) => eventListenerRef.current(event);
-    window.addEventListener('beforeunload', eventListener);
+    window.addEventListener("beforeunload", eventListener);
     return () => {
-      window.removeEventListener('beforeunload', eventListener);
+      window.removeEventListener("beforeunload", eventListener);
     };
   }, []);
-};
-
-export default useBeforeunload;
+}
